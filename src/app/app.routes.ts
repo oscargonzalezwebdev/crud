c46b034afea565e3c341d13a9b7bb9607517055a
Oscar Gonzalez <opt3.oscar@gmail.com>
2025-03-19 16:27:24 +0100

    Rutas y Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { VistaUsuarioComponent } from './pages/vista-usuario/vista-usuario.component';
 
 export const routes: Routes = [
 
-     
+    { path: "", pathMatch: "full", redirectTo: "home" },
+    { path: "home", component: UsuariosComponent },
+    { path: "usuario/:id", component: VistaUsuarioComponent },
+    { path: "**", redirectTo: "home" }
+    
 ];
